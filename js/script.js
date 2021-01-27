d6afd84ba6cbdcf4abede7793f5cd03e318a7117
emad-salah <emads14@gmail.com>
2021-01-27 19:11:31 +0100

    Added JS functionality

--- js/script.js
+++ js/script.js
@@ -1,7 +1,66 @@
-$('.info-button, .modal-background, .modal-close-button').on('click', function (e) {
-    $('.modal').toggleClass('hidden')
-})
+$(document).ready(function () {
+  const formatTime = time =>
+    time ? time.toFixed(0).toString().padStart(2, "0") : "00";
 
-$('.hamburger-menu-icon').on('click', function (e) {
-    $('.drop-down').toggleClass('active')
-})
+  $(".info-button, .modal-background, .modal-close-button").click(function (e) {
+    if ($(this).hasClass("info-button")) {
+      const card = $(this).closest(".card, .long-card");
+      const logo = card.data("logo");
+      const companyName = card.data("name");
+
+      if (logo) {
+        $(".modal-logo-button")
+          .css({
+            backgroundImage: `url(${logo})`
+          })
+          .text("");
+      } else {
+        $(".modal-logo-button")
+          .css({
+            backgroundImage: "none"
+          })
+          .text((companyName || "Company Name").toUpperCase());
+      }
+    }
+    $(".modal").toggleClass("hidden");
+  });
+
+  $(".hamburger-menu-icon").click(function (e) {
+    $(".drop-down").toggleClass("active");
+  });
+
+  const getUnit = (value, unit) =>
+    value === 1
+      ? `${value.toFixed(0)} ${unit}`
+      : `${value.toFixed(0)} ${unit}s`;
+
+  setInterval(function () {
+    $("[data-countdown-date]").each(function () {
+      const date = $(this).data("countdown-date");
+      const parsedDate = luxon.DateTime.fromISO(date);
+      const { days, hours, minutes } = parsedDate.diffNow([
+        "days",
+        "hours",
+        "minutes"
+      ]);
+
+      const desktopCountdownText = [
+        getUnit(days, "Day"),
+        getUnit(hours, "Hour"),
+        getUnit(minutes, "Minute")
+      ].join(" ");
+      const mobileCountdownText = [
+        formatTime(days),
+        formatTime(hours),
+        formatTime(minutes)
+      ].join(":");
+
+      $(this)
+        .find(".long-countdown.desktop, .countdown.desktop")
+        .text(desktopCountdownText);
+      $(this)
+        .find(".long-countdown.mobile, .countdown.mobile")
+        .text(mobileCountdownText);
+    });
+  }, 1000);
+});
